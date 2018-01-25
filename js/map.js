7fad377bd7469af65b8f48e66f62bcbf9eb34a1c
cnbzzz <cnbzzz@126.com>
2018-01-25 14:39:04 +0800

    0.1
    创建初始版本

---
+++ js/map.js
@@ -0,0 +1,19 @@
+/**
+ * This file is part of bzmap Project
+ * Created by cnbzzz (cnbzzz@126.com) on 2018/1/18 15:34
+ * Copyright (c) 2018 https://github.com/cnbzzz
+ *
+ */
+var chinaMap = (function(){
+
+    var chinaMap = function(el, options){
+        this.$el = $(el)
+
+    }
+
+    var init = function (el, options){}
+
+    return {
+        init: init
+    }
+})()
